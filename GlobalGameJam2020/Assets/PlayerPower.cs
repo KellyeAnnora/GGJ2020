884b927a26229e83cac4ac8310716a87295a343a
mb030317 <56268235+mb030317@users.noreply.github.com>
2020-02-01 12:14:30 -0500

    Update PlayerPower.cs

--- GlobalGameJam2020/Assets/PlayerPower.cs
+++ GlobalGameJam2020/Assets/PlayerPower.cs
@@ -7,30 +7,23 @@ namespace Runemark.DialogueSystem
     public class PlayerPower : MonoBehaviour
     {
 
-        public int power;
-        public int EnemyPower1;
-        public int WinChance;
+        public int playerPower;
+        public int combatPower;
 
-
-        public void WinChance1()
-        {
-            var chance = power / (power - EnemyPower1)*10;
-            WinChance = chance;
-        }
 
         public void Combat()
         {
-            if (power <= 10)
+            if (playerPower <= 10)
             {
-                var combat = Random.Range(1, power);
+                var combat = Random.Range(1, playerPower);
 
-                power = combat;
+                combatPower = combat;
             }
         }
 
         public void Update()
         {
-            DialogueSystem.SetGlobalVariable("PlayerPower", power);
+            DialogueSystem.SetGlobalVariable("PlayerPower", combatPower);
             DialogueSystem.SetGlobalVariable("WinChance", WinChance);
         }
     }
