8464d0fd2c495dc0d1ddbaa5dc189e0bb928d591
KellyeAnnora <kellyeannora@gmail.com>
2020-01-31 20:05:35 -0500

    add player controller and camera controller

---
+++ GlobalGameJam2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+
+    Camera cam;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100f, movementMask))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            //interact
+        }
+    }
+
+
+}
